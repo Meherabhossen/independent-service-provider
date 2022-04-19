442af40403d07d5810d0d0ff50688182d05e1641
Meherabhossen <meherabhossen45@gmail.com>
2022-04-20 01:50:34 +0600

    7th add

---
+++ src/Pages/Home/WildlifeInfo/WildlifeInfo.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const WildlifeInfo = () => {
+    return (
+        <div className='container align-items-center mt-5'>
+            <div>
+            <h1>Jack Stephen The photographer details </h1>
+            <p>I am a Certified wildlife photographer. I work as a wildlife Photographer since 2005. In my carrier i visit so many places such as Switzerland, Canada, Africa, some nationals parks and rain forests for get the best wildLife photo.I won the international wildlife photography award 2 times </p>
+            </div>
+            <div>
+                <img style={{width: '50%'}} src="https://images.squarespace-cdn.com/content/v1/556323dee4b006bb6875f975/1593468972080-FYA3HX81ZY0OAK1HOIKL/Jorn+-Vangoidtsenhoven-wildlife-photographer.jpg" alt="" />
+            </div>
+        </div>
+    );
+};
+
+export default WildlifeInfo;
